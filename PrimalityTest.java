009c7b8efd77b268e35d6c50246f75a7cdc9c577
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2020-12-13 20:02:33 +0530

    Add files via upload

---
+++ PrimalityTest.java
@@ -0,0 +1,27 @@
+/*Java Primality Test
+Sample Input
+13
+Sample Output
+prime
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class PrimalityTest{
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+         BigInteger number = new BigInteger(n);
+        if (number.isProbablePrime(1)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+        scanner.close();       
+    }
+}
