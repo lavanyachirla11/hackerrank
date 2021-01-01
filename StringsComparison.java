effb5bc5a0f2afc287a66c225243f25b17e176af
lavanyachirla11 <69451930+lavanyachirla11@users.noreply.github.com>
2021-01-01 20:06:06 +0530

    Update StringsComparison.java

--- StringsComparison.java
+++ StringsComparison.java
@@ -1,14 +1,4 @@
 /*
-Java Strings Introduction
-Input Format
-The first line contains a string A . The second line contains another string B. The strings are comprised of only lowercase English letters.
-
-Output Format
-There are three lines of output:
-For the first line, sum the lengths of  A and B.
-For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
-For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.
-
 Sample Input 0
 hello
 java
